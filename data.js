426f4b8563739ccea56849bdffa3dba5607c873d
Ilyas Samsudin <ilyassmsdn@gmail.com>
2023-01-15 23:20:58 +0700

    data

---
+++ data.js
@@ -0,0 +1,32 @@
+export const Data = [
+    {
+        "id" : 1,
+        "name" : "John Mayer",
+        "profession" : "singer",
+        "gender" : "male"
+    },
+    {
+        "id" : 2,
+        "name" : "Fara Queen",
+        "profession" : "model",
+        "gender" : "female"
+    },
+    {
+        "id" : 3,
+        "name" : "Asep Sudrajat",
+        "profession" : "engineer",
+        "gender" : "male"
+    },
+    {
+        "id" : 4,
+        "name" : "Agus Sumanto",
+        "profession" : "bus driver",
+        "gender" : "male"
+    },
+    {
+        "id" : 5,
+        "name" : "Nida",
+        "profession" : "teacher",
+        "gender" : "female"
+    }
+]
--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-import express from "express"
+import express, { json } from "express"
+import { Data } from "./data.js"
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json({
         data : "this is an api using express js"
     })
+})
+
+app.get('/person', (req, res) => {
+    res.json(Data)
+})
+
+app.get('/person/:id', (req, res) => {
+    const i = req.params.id
+    const id = parseInt(i)
+    const data = Data.find(c => c.id === id)
+    console.log(Data)
+    res.json({data})
+})
+
+app.post('/person', (req, res) => {
+    res.status(201).json(req.body)
+})
+
+app.delete('/person/:id', (req, res) => {
+    const id = req.params.id
+    res.json({message : `data with id = ${id} deleted successfully`})
+})
+
+app.put('/person/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const data = req.body
+    data["id"] = id
+    res.json(req.body)
+})
+
+app.patch('/person/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const dat = req.body
+    const DataCopy = JSON.parse(JSON.stringify(Data))
+    const data = DataCopy.find(c => c.id === id)
+    Object.assign(data, dat)
+    res.json(data)
 })
 
 app.listen(2000, () => console.log("running on http://localhost:2000"))
